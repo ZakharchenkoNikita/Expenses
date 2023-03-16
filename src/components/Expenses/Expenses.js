72ac906e6d374745c63fb10adc9ee9bd1507ce26
immortalprogrammist <immortalvelial@icloud.com>
2023-03-16 11:20:48 +0100

    first commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import "./Expenses.css";
+
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpenseItem from "./ExpenseItem.js";
+import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+export default Expenses;
